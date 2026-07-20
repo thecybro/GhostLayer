58b9852e9e1df3e40b6fc24cf2834ba637206c6c
thecybro <121627329+thecybro@users.noreply.github.com>
2026-07-21 00:45:00 +0545

    feat: Initiative towards adding storage parser to make persistence of information possible

--- ghost/src/storage.rs
+++ ghost/src/storage.rs
@@ -1,6 +1,22 @@
 use serde_json;
+use serde::Serialize;
 use crate::identity::Identity;
 use crate::friends::Friend;
+
+// Commented this and friend_display_from_json because FriendDisplay was identical to Friend
+// #[derive(Serialize, Deserialize)]
+// pub struct Friend {
+//     pub nickname: Option<String>,
+//     pub public_key: String,
+//     pub key_id: String,
+// }
+
+#[derive(Serialize)]
+pub struct LoadResult {
+    pub has_identity: bool,
+    pub identity_key_id: Option<String>,
+    pub friends: Vec<Friend>,
+}
 
 pub fn friend_key(public_key: &str) -> String {
     format!("friend:{public_key}")
@@ -10,23 +26,23 @@ pub fn identity_to_json(identity: &Identity) -> String {
     serde_json::to_string(identity).unwrap()
 }
 
-// pub fn identity_from_json(json: &str) -> Result<Identity, String> {
-//     match serde_json::from_str::<Identity>(json) {
-//         Ok(id) => Ok(id),
-//         Err(_) => Err( "Couldn't convert to identity from json!".to_string())
-//     }
-// }
+pub fn identity_from_json(json: &str) -> Result<Identity, String> {
+    match serde_json::from_str::<Identity>(json) {
+        Ok(id) => Ok(id),
+        Err(_) => Err( "Couldn't convert to identity from json!".to_string())
+    }
+}
 
 pub fn friend_to_json(friend: &Friend) -> String {
     serde_json::to_string(friend).unwrap()
 }
 
-// pub fn friend_from_json(json: &str) -> Result<Friend, String> {
-//     match serde_json::from_str::<Friend>(json) {
-//         Ok(fr) => Ok(fr),
-//         Err(_) => Err("Couldn't convert to friend from json!".to_string())
-//     }
-// }
+pub fn friend_from_json(json: &str) -> Result<Friend, String> {
+    match serde_json::from_str::<Friend>(json) {
+        Ok(fr) => Ok(fr),
+        Err(_) => Err("Couldn't convert to friend from json!".to_string())
+    }
+}
 
 pub fn index_to_json(index: &Vec<String>) -> String {
     serde_json::to_string(index).unwrap()
@@ -46,4 +62,46 @@ pub fn add_to_index(index: &Vec<String>, public_key: &str) -> Vec<String> {
         new_index.push(public_key.to_string())
     };
     new_index
+}
+
+// Commented this and FriendDisplay because FriendDisplay was identical to Friend
+// pub fn friend_from_json(json: &str) -> Result<Friend, String> {
+//     match serde_json::from_str::<Friend>(json) {
+//         Ok(friend) => Ok(friend),
+//         Err(_) => Err("Couldn't convert driend display from json!".to_string())
+//     }
+// }
+
+pub fn parse_storage(storage_json: String) -> LoadResult {
+    let parsed: serde_json::Value = serde_json::from_str(&storage_json).unwrap();
+
+    let identity_key_id = if !parsed["identity"].is_null() {
+        let identity_str = parsed["identity"].as_str().unwrap();
+        let identity: Identity = identity_from_json(identity_str).unwrap();
+        Some(identity.key_id)
+    } else {
+        None
+    };
+    let friends = if !parsed["friend_index"].is_null() {
+        let friend_index: Vec<String> = serde_json::from_value(parsed["friend_index"].clone()).unwrap();
+        let mut friends = Vec::new();
+        
+        for public_key in friend_index {
+            let key = friend_key(&public_key);
+            let friend_json = parsed[&key].as_str().unwrap();
+            let friend = friend_from_json(friend_json).unwrap();
+            
+            friends.push(friend);
+        }        
+        friends
+    } else {
+        Vec::new()
+    };
+    let has_identity = !parsed["identity"].is_null();
+
+    LoadResult {
+        has_identity,
+        identity_key_id,
+        friends,
+    }
 }
